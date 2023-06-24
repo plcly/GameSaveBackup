4028035f679b3af9284450cc6106b2374b31271b
miniHost <miniHost@DESKTOP>
2023-06-24 23:35:36 +0800

    init blazor hybrid

---
+++ GameSaveManagement/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GameSaveManagement
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            serviceCollection.AddMudServices();
+            var provider = serviceCollection.BuildServiceProvider();
+            Resources.Add("services", provider);
+        }
+    }
+}
